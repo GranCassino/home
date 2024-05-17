6d66e16f14e5d1fff80b1603ac78e163220e7051
GranCassino <160289700+GranCassino@users.noreply.github.com>
2024-05-16 22:37:11 -0300

    Update pesquisar.js

--- pesquisar.js
+++ pesquisar.js
@@ -75,6 +75,7 @@ function searchImages() {
       { src: 'https://static.gambling-malta.com/casino/CasinoGamePlaytech/GameId_456/Background_1615468400.jpg', alt: 'Premium Blackjack', link: 'premium-blackjack' },
       { src: 'https://static.gambling-malta.com/casino/CasinoGameGamomat/GameId_2665/Background_1623142351.jpg', alt: 'Ramses Book', link: 'ramses-book' },
       { src: 'https://static.gambling-malta.com/casino/CasinoGameBlueprint/GameId_2268/Background_1623061202.jpg', alt: 'Rome Rise Of An Empire', link: 'rome-rise-of-an-empire' },
+      { src: 'https://static.gambling-malta.com/casino/CasinoGameBooming%20Games/GameId_14733/Thumbnail_1713859141721.jpg', alt: 'Ronaldinho Spins', link: 'ronaldinho-spins' },
       { src: 'https://static.gambling-malta.com/casino/CasinoGamePragmaticPlay/GameId_3648/Background_1619527774.jpg', alt: 'Roulette', link: 'roulette' },
       { src: 'https://i.imgur.com/m3Nu9cV.png', alt: 'Roulette BP Aovivo', link: 'roulette-bp-aovivo' },
       { src: 'https://i.imgur.com/QDhLki8.png', alt: 'Roulette Fazi Aovivo', link: 'roulette-fazi-aovivo' },
